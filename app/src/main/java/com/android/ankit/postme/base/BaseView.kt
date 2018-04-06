c5835a6a42d9ec9e552b8e34de236b6a9e057abd
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-04-06 15:00:57 +0800

    Added BaseActivity Added BasePresenter Added BaseView

--- app/src/main/java/com/android/ankit/postme/base/BaseView.kt
+++ app/src/main/java/com/android/ankit/postme/base/BaseView.kt
@@ -1,5 +1,15 @@
 package com.android.ankit.postme.base
 
+import android.content.Context
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 05/04/2018 (MM/DD/YYYY )
  */
+
+interface BaseView {
+    /**
+     * Returns the Context in which the application is running.
+     * @return the Context in which the application is running
+     */
+    fun getContext(): Context
+}
