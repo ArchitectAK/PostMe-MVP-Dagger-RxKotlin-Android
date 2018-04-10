ebf564cae42c42e1e7e4e0b9dd6b078afe500507
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-04-10 11:03:44 +0800

    Added Modules added dependencies

--- app/src/main/java/com/android/ankit/postme/injection/component/PresenterInjector.kt
+++ app/src/main/java/com/android/ankit/postme/injection/component/PresenterInjector.kt
@@ -1,7 +1,33 @@
 package com.android.ankit.postme.injection.component
 
+import com.android.ankit.postme.base.BaseView
+import com.android.ankit.postme.injection.module.ContextModule
+import com.android.ankit.postme.injection.module.NetworkModule
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 05/04/2018 (MM/DD/YYYY )
  */
+
+@Singleton
+@Component(modules = [(ContextModule::class), (NetworkModule::class)])
 interface PresenterInjector {
+    /**
+     * Injects required dependencies into the specified PostPresenter.
+     * @param postPresenter PostPresenter in which to inject the dependencies
+     */
+    fun inject(postPresenter: PostPresenter)
+
+    @Component.Builder
+    interface Builder {
+        fun build(): PresenterInjector
+
+        fun networkModule(networkModule: NetworkModule): Builder
+        fun contextModule(contextModule: ContextModule): Builder
+
+        @BindsInstance
+        fun baseView(baseView: BaseView): Builder
+    }
 }
--- app/src/main/java/com/android/ankit/postme/injection/module/NetworkModule.kt
+++ app/src/main/java/com/android/ankit/postme/injection/module/NetworkModule.kt
@@ -1,5 +1,42 @@
 package com.android.ankit.postme.injection.module
 
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 05/04/2018 (MM/DD/YYYY )
  */
+@Module
+object NetworkModule {
+    /**
+     * Provides the Post service implementation.
+     * @param retrofit the Retrofit object used to instantiate the service
+     * @return the Post service implementation.
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun providePostApi(retrofit: Retrofit): PostAPI {
+        return retrofit.create(PostAPI::class.java)
+    }
+
+    /**
+     * Provides the Retrofit object.
+     * @return the Retrofit object
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .build()
+    }
+}
