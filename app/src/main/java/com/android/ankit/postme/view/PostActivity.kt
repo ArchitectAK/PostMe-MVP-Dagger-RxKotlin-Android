39ca2418957793ccd80a93797e493062b3eec3df
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-04-10 13:39:33 +0800

    Added Views

---
+++ app/src/main/java/com/android/ankit/postme/view/PostActivity.kt
@@ -0,0 +1,63 @@
+package com.android.ankit.postme.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.android.ankit.postme.R
+import com.android.ankit.postme.base.BaseActivity
+import com.android.ankit.postme.presenter.PostPresenter
+
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 05/04/2018 (MM/DD/YYYY )
+ */
+
+
+class PostActivity  : BaseActivity<PostPresenter>(), PostView {
+    /**
+     * DataBinding instance
+     */
+    private lateinit var binding: ActivityPostBinding
+
+    /**
+     * The adapter for the list of posts
+     */
+    private val postsAdapter = PostAdapter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_post)
+        binding.adapter = postsAdapter
+        binding.layoutManager = LinearLayoutManager(this)
+        binding.dividerItemDecoration = DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+
+        presenter.onViewCreated()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onViewDestroyed()
+    }
+
+    override fun updatePosts(posts: List<Post>) {
+        postsAdapter.updatePosts(posts)
+    }
+
+    override fun showError(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+    }
+
+    override fun showLoading() {
+        binding.progressVisibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        binding.progressVisibility = View.GONE
+    }
+
+    override fun instantiatePresenter(): PostPresenter {
+        return PostPresenter(this)
+    }
+}
